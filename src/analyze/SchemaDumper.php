cb7816d7e93f12442d44a45cb9e64b9ab6c4e736
Kevin <217211989+kevinsleegerslameco@users.noreply.github.com>
2026-04-25 22:46:44 +0200

    fix(02-03): drop unused Craft import in SchemaDumper

--- src/analyze/SchemaDumper.php
+++ src/analyze/SchemaDumper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace lameco\kunstmaanmigrator\analyze;
 
-use Craft;
 use lameco\kunstmaanmigrator\Plugin;
 use lameco\kunstmaanmigrator\filter\MigrationFilters;
 use yii\base\Component;
